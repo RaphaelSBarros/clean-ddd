714ed410a6e80cce234c485622e4848057e4996c
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-09-26 13:37:43 -0300

    Classe base de AggregateRoot

---
+++ src/core/entities/aggreagate-root.ts
@@ -0,0 +1,3 @@
+import { Entity } from './entity'
+
+export abstract class AggreagateRoot<Props> extends Entity<Props> {}
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,5 +1,5 @@
+import { AggreagateRoot } from '@/core/entities/aggreagate-root'
 import { Slug } from './value-objects/slug'
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
@@ -14,7 +14,7 @@ export interface QuestionProps {
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggreagateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId
   }
